813c52eaeceb1c99871360d01d8ac8b2ed9b73a1
jim_dox <jdoxtader@protonmail.com>
2020-05-16 16:17:26 -0400

    reformatted dir structure

---
+++ include/RocketMath.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "../src/DLList.hpp"
+#include "../src/Vec4d.hpp"
+
+/* author: James Doxtader */
+/* RocketMath.h: include file to incorporate core library into another application */
+
